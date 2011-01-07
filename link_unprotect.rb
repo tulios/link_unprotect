fea5175a6ab088193fbad93cb7c7fb20dabb6c2b
tulios <ornelas.tulio@gmail.com>
2011-01-06 23:07:14 -0200

    adding copy to clipboard feature

--- link_unprotect.rb
+++ link_unprotect.rb
@@ -9,6 +9,10 @@ require "base64"
 set :views, lambda { File.join(root, "views") }
 set :public, lambda { File.join(root, "public") }
 
+# ==========================================================
+# Actions
+# ==========================================================
+
 get '/' do
   erb :index
 end
@@ -22,4 +26,34 @@ post '/' do
     puts 'nenhum caso!'
   end
   erb :index
+end
+
+# ==========================================================
+# Helpers
+# ==========================================================
+
+helpers do
+  def get_clippy text, bgcolor = '#FFFFFF'
+    %Q{
+      <object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="110" height="14" id="clippy" >
+          <param name="movie" value="/flash/clippy.swf"/>
+          <param name="allowScriptAccess" value="always" />
+          <param name="quality" value="high" />
+          <param name="scale" value="noscale" />
+          <param NAME="FlashVars" value="text=#{text}">
+          <param name="bgcolor" value="#{bgcolor}">
+          <embed src="/flash/clippy.swf"
+                 width="110"
+                 height="14"
+                 name="clippy"
+                 quality="high"
+                 allowScriptAccess="always"
+                 type="application/x-shockwave-flash"
+                 pluginspage="http://www.macromedia.com/go/getflashplayer"
+                 FlashVars="text=#{text}"
+                 bgcolor="#{bgcolor}"
+          />
+          </object>
+    }
+  end
 end
